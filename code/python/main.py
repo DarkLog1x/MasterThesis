4c7f9d4b3cbcce789204d0ecb16176687ba79217
Aleksander Okonski <aleksander.oko@gmail.com>
2017-02-17 17:09:04 +0100

    again need to work on the environment variables

--- code/python/main.py
+++ code/python/main.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python
+import sys
+import time
+import os
+import sys
+import inspect
+from os import environ as env
+import subprocess
 
-print('Hello World!')
+from novaclient import client
+import keystoneclient.v3.client as ksclient
+from keystoneauth1 import loading
+from keystoneauth1 import session
+
+loader = loading.get_plugin_loader('password')
+auth = loader.load_from_options(auth_url=env['OS_AUTH_URL'],
+                                username=env['OS_USERNAME'],
+                                password=env['OS_PASSWORD'],
+                                project_name=env['OS_PROJECT_NAME'],
+                                user_domain_name=env['OS_USER_DOMAIN_NAME'],
+                                project_domain_name=env['OS_PROJECT_DOMAIN_NAME'])
+
+sess = session.Session(auth=auth)
+nova = client.Client('2.1', session=sess)
+nova.servers.list()
+
+# def main(argv):
+
+# print('Hello World!')
+# pass
+
+# if __name__ == "__main__":
+# main(sys.argv)
